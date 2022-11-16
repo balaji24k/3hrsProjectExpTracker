f1ff85bc6e37c7dfa6038e270368261c8694a0cf
Balaji <112850658+iambalaji325@users.noreply.github.com>
2022-11-16 17:28:34 +0530

    minimum of Array

--- maxArray.py
+++ maxArray.py
@@ -1,10 +1,16 @@
 arr = [5,-1,2,6,9,7]
 arr.append(12)
+min = arr[0]
 max = arr[0]
 
 for i in range (0,len(arr)):
-    if max<arr[i]:
-        max=arr[i]
+    if max < arr[i]:
+        max = arr[i]
         
-print(max)
+    if min > arr[i]:
+        min = arr[i]
+        
+        
+print("maximum:",max)
+print("minimum:",min)
   
